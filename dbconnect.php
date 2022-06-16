5ff1a2ae15b5b484c750a6e3ff88da4c69336b04
rizwan <rizwan@hp.local>
2022-06-16 23:48:22 +0530

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,40 @@
+<?php
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "services";
+
+    // Database connection
+    $connection = mysqli_connect($server, $username, $password,$dbname);
+
+    if (!$connection) {
+        die("Connect to the database is not established" . mysqli_connect_error());
+    }
+
+	$tvName = $_POST['tv'];
+	$tvproblem = $_POST['problem'];
+	$clientAddress = $_POST['address'];
+	$clientNumber = $_POST['phno'];
+
+	
+    $query = "INSERT INTO `tvservice` (`tvname`, `tvproblem`, `clientaddress`, `contact`) VALUES ('$tvName','$tvproblem','$clientAddress','$clientNumber')";
+
+
+    if($connection->query($query)==true){
+        // $insert = true;
+        echo "Good God your services is registerd and our worker will call you soon...";
+    }
+    else{
+        echo "<p>Error : $query <br> $connection->error</p>";
+    }
+
+    $connection->close();
+
+?>
+
+<html>
+    <body>
+        <h1>Your service is succesfully booked</h1>
+    </body>
+</html>
